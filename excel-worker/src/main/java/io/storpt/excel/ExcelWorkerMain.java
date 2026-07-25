e40bba03e04dd749a04063adac06901eabce476a
verbang <verbang@users.noreply.github.com>
2026-07-25 22:28:31 +0800

    fix: preserve worker protocol in shaded jar

--- excel-worker/src/main/java/io/storpt/excel/ExcelWorkerMain.java
+++ excel-worker/src/main/java/io/storpt/excel/ExcelWorkerMain.java
@@ -5,6 +5,7 @@ public final class ExcelWorkerMain {
   private ExcelWorkerMain() {}
 
   public static void main(String[] args) {
+    System.setProperty("log4j2.statusLoggerLevel", "OFF");
     int exitCode = new WorkerCli().run(System.in, System.out);
     if (exitCode != 0) {
       System.exit(exitCode);
